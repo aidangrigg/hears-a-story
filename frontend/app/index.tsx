9a1da0617901a622a1c9e36ddc7571703f55e7de
aidangrigg <aidangrigg02@gmail.com>
2024-09-17 20:27:06 +1000

    TTS example using Expo Speech

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,10 @@
-import { Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
+import * as Speech from 'expo-speech';
+import { useState } from "react";
 
 export default function Index() {
+  const [text, setText] = useState("");
+
   return (
     <View
       style={{
@@ -9,7 +13,13 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput
+	placeholder="Text to speak!"
+	value={text}
+	onChangeText={setText}/>
+      <Button
+	title="Client-side native TTS"
+	onPress={() => Speech.speak(text)}/>
     </View>
   );
 }
