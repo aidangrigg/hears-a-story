4247b489cc70eedf71b24c137f9f3a38cfa66c5a
aidangrigg <aidangrigg02@gmail.com>
2024-09-18 15:37:37 +1000

    Server side TTS error handling

--- server/src/api/tts/tts.handler.ts
+++ server/src/api/tts/tts.handler.ts
@@ -8,7 +8,7 @@ class SpeechSynth {
   static instance: Promise<TextToAudioPipelineType> | undefined = undefined;
 
   static async getInstance(progress_callback = undefined) {
-    if(!this.instance) {
+    if (!this.instance) {
       this.instance = pipeline('text-to-speech', 'Xenova/speecht5_tts', { quantized: false, progress_callback });
     }
     return this.instance;
@@ -16,16 +16,40 @@ class SpeechSynth {
 }
 
 export async function postTTS(req: Request, res: Response) {
-  const synth = await SpeechSynth.getInstance()
+  let synth: TextToAudioPipelineType;
+  try {
+    synth = await SpeechSynth.getInstance();
+  } catch (_) {
+    res.status(500)
+      .send("Internal server error, failed to load TTS model");
+    return;
+  }
 
-  console.log(req.body);
+  const text: string | undefined = req.body.text;
 
-  const text = req.body.text | "hello, your text did not send" ;
-  
-  const result = await synth(req.body.text, { speaker_embeddings });
+  if(typeof text !== "string") {
+    res.status(400)
+      .send("TTS input must be passed in the \"text\" field of the json body.");
+    return;
+  }
+
+  if(text.length <= 0) {
+    res.status(400)
+      .send("Text cannot be empty.");
+    return;
+  }
+
+  let result;
+  try {
+    result = await synth(text, { speaker_embeddings });
+  } catch (_) {
+    res.status(500)
+      .send("Internal server error, failed to parse TTS input");
+    return;
+  }
 
   const wav = encodeWAV(result.audio, result.sampling_rate);
 
   res.set('Content-Type', 'audio/wav');
   res.send(Buffer.from(wav));
-};
+}
