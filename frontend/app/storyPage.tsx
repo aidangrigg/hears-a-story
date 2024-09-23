b6350bd03ac246a1a42012ea2561e8e2af11dd74
JulianUTS <julian.r.arceo@student.uts.edu.au>
2024-09-23 15:23:34 +1000

    Textbox array and Input box implented
    Created an array for textboxes that automatically update the screen when a new textbox is added.
    Created the Input box component for the user prompts.

--- frontend/app/storyPage.tsx
+++ frontend/app/storyPage.tsx
@@ -2,15 +2,37 @@ import { Text, View } from "react-native";
 import { StyleSheet, Image, Platform } from 'react-native';
 import NarratorTextbox from '@/components/NarratorTextbox';
 import UserTextbox from '@/components/UserTextbox';
+import InputBox from "@/components/InputBox";
+import React from 'react';
 
-interface textBox{
+class textBox{
   id: number;
   pov: boolean;
   text: string;
+
+  constructor(id: number,  pov: boolean, text: string) {
+    this.id = id;
+    this.pov = pov;
+    this.text = text;
+  }
 }
 
-let text1 = new textBox(id: 01, pov: false, text: "dwadawdawda");
-const textBoxes: textBox[] = []
+let text1 = new textBox(1, false, "TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText");
+let text2 = new textBox(2, true, "TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText");
+let text3 = new textBox(3, true, "TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText");
+let text4 = new textBox(4, false, "TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText");
+
+const textBoxes: textBox[] = [text1, text2, text3, text4];
+
+const settextBoxes = textBoxes.map(textBox => {
+  if(textBox.pov){
+    return <UserTextbox text = {textBox.text}/>;
+  };
+  return <NarratorTextbox text = {textBox.text}/>;
+    
+});
+
+
 
 
 let text = "TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText";
@@ -18,8 +40,8 @@ let storyName: string = "Story Name";
 export default function Index() {
   return (
     <View style={styles.pageStyle}>
-      <NarratorTextbox text = {text}/>
-      <UserTextbox text = {text}/>
+      {settextBoxes}
+    <InputBox />
     
 
    
@@ -32,6 +54,7 @@ const styles = StyleSheet.create({
   
   pageStyle:{
     flex: 1,
+    position: 'relative',
     backgroundColor: 'rgba(25, 38, 55, 1)',
     borderColor: 'white',
     borderStyle: "solid",
