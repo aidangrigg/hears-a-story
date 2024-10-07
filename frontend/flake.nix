c133e391d864da658f3951bd18bfe9bab9866ade
Aidan Grigg <aidangrigg02@gmail.com>
2024-10-07 13:49:57 +1100

    Simple nix devshell

---
+++ frontend/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            typescript
+            nodePackages.typescript-language-server
+          ];
+        };
+      });
+}
