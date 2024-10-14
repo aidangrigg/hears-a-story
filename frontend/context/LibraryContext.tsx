8d9fee76e9841113d5c4503aa9f676ee8ddce989
Jack-Richard <13893716@student.uts.edu.au>
2024-10-14 17:22:46 +1100

    Updated mock default data

--- frontend/context/LibraryContext.tsx
+++ frontend/context/LibraryContext.tsx
@@ -15,10 +15,9 @@ import { Story } from "@/types/Story";
 
   export const LibraryProvider = ({ children }: any) => {
     const [library, updateLibrary] = useState<Story[]>([
-      {key: 0, title: "Story 1", status: "Ongoing", duration: "2h 3m"},
-      {key: 1, title: "Story 2", status: "Ongoing", duration: "2h 3m"},
-      {key: 2,  title: "Story 3", status: "Ongoing", duration: "2h 3m"},
-      {key: 3,  title: "The Hobbit", status: "Ongoing", duration: "2h 3m"}
+      {key: 0, title: "Story 1", status: "Ongoing", duration: "short", genre: "crime", allowAdultContent: false},
+      {key: 1, title: "Story 2", status: "Ongoing", duration: "medium", genre: "crime", allowAdultContent: false},
+      {key: 2,  title: "Story 3", status: "Ongoing", duration: "long", genre: "crime", allowAdultContent: false},
     ]);
   
     const addStory = (newStory: Story) => {
